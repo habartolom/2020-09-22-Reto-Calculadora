0dd33bbbc0d1c1ff27c61a1d9993044e1056fd40
Harold Andrrés Bartolo Moscoso <habartolom@gmail.com>
2020-09-24 23:55:09 -0500

    Ajustes eventos

--- assets/scritps/script.js
+++ assets/scritps/script.js
@@ -9,31 +9,39 @@ const equal = document.getElementById('equal');
 const registry = document.getElementById('registry');
 const operationsHistory = document.getElementById('operationsHistory');
 const resultHistory = document.getElementById('resultHistory');
+const unaryOps = document.getElementsByClassName('unary');
 
 let queueOps = [];
 let entryConcatenable = false;
 let newRecord = true;
 
-// Event Listener para teclas numéricas
+// Event Listener para botones numéricos
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', () => {
+        if (newRecord)
+            operationsScreen.innerText = '';
+        if(queueOps.length % 2 == 1)
+            operationsScreen.innerText = '';
+
         validateEntry(numbers[i].innerText);
     })
 }
 
-// Event Listener para tecla CE
+// Event Listener para botón CE
 clearEntry.addEventListener('click', () => {
     entry.innerText = '0';
+    if (newRecord)
+        operationsScreen.innerText = '';
 });
 
-// Event Listener para tecla C
+// Event Listener para botón C
 clear.addEventListener('click', () => {
     entry.innerText = '0';
     operationsScreen.innerText = '';
     queueOps = [];
 });
 
-// Event Listener para tecla Backspace
+// Event Listener para botón Backspace
 backSpace.addEventListener('click', () => {
     if (entryConcatenable) {
         let number = entry.innerText;
@@ -45,71 +53,112 @@ backSpace.addEventListener('click', () => {
     }
 });
 
-// Event Listener para teclas de operaciones binarias
-for (let i = 0; i < binaryOps.length; i++) {
-    let operator = binaryOps[i];
-    operator.addEventListener('click', () => {
-        if (entryConcatenable) {
-            operationsScreen.innerText += ' ' + entry.innerText;
-            operationsScreen.innerText += ' ' + operator.innerText;
-            entry.innerText = makeOperations(entry.innerText, operator.innerText);
-            entryConcatenable = false;
-        }
-        else {
-            if (newRecord) {
-                operationsScreen.innerText = entry.innerText;
-                operationsScreen.innerText += ' ' + operator.innerText;
-                entry.innerText = makeOperations(entry.innerText, operator.innerText);
-                newRecord = false;
-            }
-            else {
-                let opsArray = operationsScreen.innerText.split(' ');
-                opsArray[opsArray.length - 1] = operator.innerText;
-                operationsScreen.innerText = opsArray.join(' ');
-                queueOps.pop();
-                queueOps.push(operator.innerText);
-            }
-        }
+// Event Listener para los botones de operaciones Unarias.
+for (let i = 0; i < unaryOps.length; i++){
+    let unaryOp = unaryOps[i];
+    unaryOp.addEventListener('click', () => {
+        let numberInEntry = parseFloat(entry.innerText);
+        let uoperator = unaryOp.id;
+        let result = doAlgebraicOperation(numberInEntry, uoperator);
 
+        if(queueOps.length % 2 == 1){
+            queueOps.pop();
+        }
+        operationsScreen.innerText += ' ' + uoperator + '(' + numberInEntry + ')';
+        
+        entry.innerText = result;
+
+        entryConcatenable = false;
+        queueOps.push(result);
     });
 }
 
-// Event Listener para la tecla '='
-equal.addEventListener('click', () => {
-    if (entryConcatenable && queueOps.length != 0) {
-        operationsScreen.innerText += ' ' + entry.innerText;
-        operationsScreen.innerText += ' ' + equal.innerText;
+// Event Listener para los botones de operaciones Binarias.
+for (let i = 0; i < binaryOps.length; i++) {
+    let binaryOp = binaryOps[i];
 
-        let num2 = parseFloat(entry.innerText);
-        let binaryOp = queueOps.pop();
-        let num1 = parseFloat(queueOps.pop());
-        entry.innerText = doArithmeticOperations(num1, num2, binaryOp);
+    binaryOp.addEventListener('click', () => {
+        const isUnaryOperation = queueOps.length % 2 != 0;
+        let numberInEntry = parseFloat(entry.innerText);
+        let newOperator = binaryOp.innerText;
+
+        if (queueOps.length == 0) {
+            queueOps.push(numberInEntry);
+            queueOps.push(newOperator);
+            operationsScreen.innerText += ' ' + numberInEntry + ' ' + newOperator;
+        }
+        else if (queueOps.length == 1) {
+            queueOps.push(newOperator);
+            operationsScreen.innerText += ' ' + newOperator;
+        }
+        else {
+            if (isUnaryOperation)
+                queueOps.pop();
+
+            let oldOperator = queueOps.pop();
+            let number = queueOps.pop();
+            let result = doArithmeticOperations(number, numberInEntry, oldOperator);
+            queueOps.push(result);
+            queueOps.push(newOperator);
+            entry.innerText = result;
+
+            if (isUnaryOperation)
+                operationsScreen.innerText += ' ' + newOperator;
+            else
+                operationsScreen.innerText += ' ' + numberInEntry + ' ' + newOperator;
+        }
 
         entryConcatenable = false;
-        newRecord = true;
-        registry.innerHTML = `
-            <div class="record">
-                <div class="operations" id="operationsHistory">${operationsScreen.innerHTML}</div>
-                <div class="accumulator history" id="resultHistory">${entry.innerHTML}</div>
-            </div>` + registry.innerHTML;
-    }
-});
+    });
+}
 
+// Event Listener para el boton igual
+equal.addEventListener('click', ()=>{
+    const isUnaryOperation = queueOps.length % 2 != 0;
+    let numberInEntry = parseFloat(entry.innerText);
+    let equalKey = equal.innerText;
+
+    if(queueOps.length == 0){
+        operationsScreen.innerText = ' ' + numberInEntry + ' ' + equalKey;
+    }
+    else if(queueOps.length == 1){
+        operationsScreen.innertext += ' ' + equalKey;
+        entry.innerText = queueOps.pop();
+    }
+    else{
+        if(isUnaryOperation){
+            queueOps.pop();
+        }
+        let oldOperator = queueOps.pop();
+        let number = queueOps.pop();
+        let result = doArithmeticOperations(number, numberInEntry, oldOperator);
+        entry.innerText = result;
+
+        if (isUnaryOperation)
+            operationsScreen.innerText += ' ' + equalKey;
+        else
+            operationsScreen.innerText += ' ' + numberInEntry + ' ' + equalKey;
+    }
+    
+    entryConcatenable = false;
+    newRecord = true;
+    registry.innerHTML = `
+        <div class="record">
+            <div class="operations" id="operationsHistory">${operationsScreen.innerHTML}</div>
+            <div class="accumulator history" id="resultHistory">${entry.innerHTML}</div>
+        </div>` + registry.innerHTML;
+});
 
 // Función que revisa el caracter ingresado al oprimir una de las teclas numéricas
 function validateEntry(character) {
 
     let storedNumber = '0';
-    
+
     if (entryConcatenable)
         storedNumber = entry.innerText;
-    
-    entryConcatenable = true;
 
-    if(newRecord){
-        operationsScreen.innerText = '';
-        newRecord = false;
-    }
+    entryConcatenable = true;
+    newRecord = false;
 
     // Si lo almacenado no tiene '.'
     if (storedNumber.indexOf('.') == -1) {
@@ -133,23 +182,23 @@ function validateEntry(character) {
     entry.innerText = storedNumber;
 }
 
-function makeOperations(number, operator) {
+function doAlgebraicOperation(number, operator) {
     let result;
-    if (queueOps.length != 0) {
-        let num2 = parseFloat(number);
-        let binaryOp = queueOps.pop();
-        let num1 = parseFloat(queueOps.pop());
-
-        result = doArithmeticOperations(num1, num2, binaryOp);
-        queueOps.push(result);
-        queueOps.push(operator)
+    switch (operator) {
+        case 'percentage':
+            result = number / 100;
+            break;
+        case 'inverse':
+            result = 1 / number;
+            break;
+        case 'sqr':
+            result = Math.pow(number, 2);
+            break;
+        case 'sqRoot':
+            result = Math.sqrt(number);
+            break;
     }
-    else {
-        result = number;
-        queueOps.push(result);
-        queueOps.push(operator);
-    }
-    return result;
+    return parseFloat(result.toPrecision(16));
 }
 
 function doArithmeticOperations(num1, num2, binaryOp) {
@@ -168,5 +217,5 @@ function doArithmeticOperations(num1, num2, binaryOp) {
             result = num1 / num2;
             break;
     }
-    return parseFloat(result.toPrecision(15));
+    return parseFloat(result.toPrecision(16));
 }
